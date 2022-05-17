fe3dac52adb3b9e8bce07375fa30701980ce01e4
EADQ <easoftwareengineer@gmail.com>
2022-05-16 23:41:28 -0600

    Adding js to the project

--- app.js
+++ app.js
@@ -53,5 +53,38 @@ let currentItem = 0;
 
 //load initial item
 window.addEventListener("DOMContentLoaded", function (){
-  console.log("shake and bake");
+showPerson();
+})
+
+function showPerson(){
+  const item = reviews[currentItem];
+  img.src = item.img;
+  author.textContent = item.name;
+  job.textContent = item.job;
+  info.textContent = item.text;
+}
+
+// show the next person
+
+nextBtn.addEventListener("click", function (){
+  currentItem++;
+  if (currentItem > reviews.length -1){
+    currentItem = 0;
+  }
+  showPerson();
+})
+
+preBtn.addEventListener("click", function (){
+  currentItem--;
+  if (currentItem < 0){
+    currentItem = reviews.length -1;
+  }
+  showPerson();
+})
+
+// show random person 
+randomBtn.addEventListener("click", function(){
+  currentItem = Math.floor(Math.random() * reviews.length);
+  console.log(currentItem);
+  showPerson();
 })
